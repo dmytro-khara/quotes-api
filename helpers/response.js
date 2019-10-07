5d03999401e89950377b76f73ac75899f7e326d9
Khara Dmytro <kharadmytro@Kharas-MacBook-Pro.local>
2019-10-07 17:10:43 +0300

    amount rounding add

--- helpers/response.js
+++ helpers/response.js
@@ -2,11 +2,12 @@ const createResponseFromQuote =  (quotes, code, amount) => {
 
     let parsedQuote = JSON.parse(quotes);
     let rate = parsedQuote.rates[code];
+    let result = amount * rate;
 
     return {
         exchange_rate: rate.toFixed(3).toString(),
         currency_code: code,
-        amount: amount * rate
+        amount: result.toFixed(0)
     }
 };
 
